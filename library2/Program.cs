77828e3943f9e68ad846446dbf588b4e38a25d9b
79141 <79141@DESKTOP-GMGD17F>
2024-04-06 19:32:53 +0900

    Add project files.

---
+++ library2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace library2
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			string connection_string = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=\"Library PD_321\";Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+			Library library = new Library(connection_string);
+			library.InsertAuthor("Abercrombie", "Joe");
+			//library.SelectAuthors();
+			library.SelectBooks("James Cameron");
+			library.InsertBook(int, string, int, decimal);
+		}
+	}
+}
---
+++ library2/library.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+
+namespace library2
+{
+	internal class Library
+	{
+		string connection_string;
+		SqlConnection connection;
+		public SqlCommand cmd { get; set; }
+
+		public Library(string connection_string) {
+			this.connection_string = connection_string;
+			connection = new SqlConnection(connection_string);
+		}
+		~Library() => connection.Close();
+		public void InsertAuthor(string last_name, string first_name)
+		{
+			try 
+			{
+				connection.Open();
+				string command = $@"
+				IF NOT EXISTS (SELECT author_id FROM Authors WHERE last_name = '{last_name}' AND first_name = '{first_name}') 
+				BEGIN	
+						INSERT INTO Authors(last_name, first_name)
+						VALUES ('{last_name}', '{first_name}')
+				END";
+				cmd = new SqlCommand(command, connection);
+				cmd.ExecuteNonQuery();
+			}
+			finally
+			{
+				if(connection!=null)connection.Close();
+			}
+		}
+		public void InsertBook(int author, string title, int pages, decimal price) {
+			try
+			{
+				connection.Open();
+				string command = $@"
+				IF NOT EXISTS (SELECT title FROM Books WHERE title = '{title}') 
+				BEGIN	
+						INSERT INTO Books(author, title, pages, price)
+						VALUES ('{author}', '{title}', '{pages}','{price}')
+				END";
+				cmd = new SqlCommand(command, connection);
+				cmd.ExecuteNonQuery();
+			}
+			finally
+			{
+				if (connection != null) connection.Close();
+			}
+		}
+		public void SelectAuthors()
+		{
+			try 
+			{
+				connection.Open();
+				string command = "SELECT * FROM Authors";
+				cmd = new SqlCommand(command, connection);
+				SqlDataReader reader = cmd.ExecuteReader();
+				Console.WriteLine($"{reader.GetName(0).PadRight(10)} {reader.GetName(1).PadRight(15)} {reader.GetName(2).PadRight(15)}");
+				while (reader.Read())
+				{
+					Console.WriteLine($"{reader[0].ToString().PadRight(10)} {reader[1].ToString().PadRight(15)} {reader[2].ToString().PadRight(15)}");
+                }
+			}
+			finally 
+			{
+				if (connection != null) connection.Close();
+			}
+		}
+		public void SelectBooks(string AuthorName = "")
+		{
+			try
+			{
+				connection.Open();
+				string command = $@"
+				SELECT 
+					title AS Title,
+					[Author] = FORMATMESSAGE('%s %s', first_name, last_name)  
+				FROM Books
+				JOIN Authors ON author=author_id
+				WHERE CONCAT(first_name, ' ', last_name) = '{AuthorName}'";
+				cmd = new SqlCommand(command, connection);
+				SqlDataReader reader = cmd.ExecuteReader();
+				Console.WriteLine($"{reader.GetName(0).ToString().PadRight(32)} {reader.GetName(1).ToString().PadRight(32)}");
+				while (reader.Read()) {
+					Console.WriteLine($"{reader[0].ToString().PadRight(32)} {reader[1].ToString().PadRight(32)}");
+				}
+			}
+			finally 
+			{
+				if (connection != null) connection.Close();
+			}
+		}
+
+	}
+}
